ff3de13bddb0a156ef6ac49f9906f423d6ef067e
jcmc3 <jcmc3@10.232.44.141>
2026-03-15 22:36:42 -0700

    Datos faltantes

--- src/main/java/OrdenamientoInserseccion/Ordenamiento_Inserccion.java
+++ src/main/java/OrdenamientoInserseccion/Ordenamiento_Inserccion.java
@@ -21,17 +21,17 @@ public class Ordenamiento_Inserccion {
     }
 
     public static void ordenarPorInsercion(int[] arr) {
-        int n = arr.length;
+        int n = arr.length; //1
         
-        for (int i = 1; i < n; i++) {
-            int llave = arr[i]; 
-            int j = i - 1;
+        for (int i = 1; i < n; i++) { //1 + n + (n-1) = 2n
+            int llave = arr[i]; //n-1
+            int j = i - 1; //n-1 
 
-            while (j >= 0 && arr[j] > llave) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
+            while (j >= 0 && arr[j] > llave) { // 2 * [(n^2 - n) / 2]
+                arr[j + 1] = arr[j];    // 2 * [(n^2 - n) / 2]
+                j = j - 1;  // 2 * [(n^2 - n) / 2]
             }
-            arr[j + 1] = llave;
+            arr[j + 1] = llave; //n-1
         }
     }
 
@@ -43,3 +43,14 @@ public class Ordenamiento_Inserccion {
     }
 }
 
+/* EXPRESIÓN ALGEBRAICA FINAL: 
+       T(n) = 1 + 2n + 3(n-1) + 3[(n^2 - n) / 2]
+       Simplificando los términos dominantes para el reporte:
+       T(n) = 1.5n^2 + 3.5n - 2
+       
+       NOTACIÓN ASINTÓTICA:
+       O(n^2) 
+       Justificación: El término de mayor orden en la expresión algebraica es n^2, 
+       lo que indica que el tiempo de ejecución crece de forma cuadrática 
+       respecto al tamaño de la entrada. 
+*/
